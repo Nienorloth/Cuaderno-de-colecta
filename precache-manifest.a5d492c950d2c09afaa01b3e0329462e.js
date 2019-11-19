1a86b91e36021ca1e35fe4c404dc52860492647d
Nienorloth <nienorloth@gmail.com>
2019-11-19 16:39:06 -0600

    Updates

---
+++ precache-manifest.a5d492c950d2c09afaa01b3e0329462e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17c0994e79d1cee0d97978aa12521340",
+    "url": "/Cuaderno-de-colecta/index.html"
+  },
+  {
+    "revision": "0439642c427ee280f424",
+    "url": "/Cuaderno-de-colecta/static/css/main.6188ff0e.chunk.css"
+  },
+  {
+    "revision": "056105ca043a2d8d3f9a",
+    "url": "/Cuaderno-de-colecta/static/js/2.98c1297c.chunk.js"
+  },
+  {
+    "revision": "0439642c427ee280f424",
+    "url": "/Cuaderno-de-colecta/static/js/main.b8d19e35.chunk.js"
+  },
+  {
+    "revision": "fef99ebbd83861c67c89",
+    "url": "/Cuaderno-de-colecta/static/js/runtime~main.ec57e481.js"
+  },
+  {
+    "revision": "04e119fa7f6a4b8def423335fdd54383",
+    "url": "/Cuaderno-de-colecta/static/media/Naturalist.04e119fa.png"
+  },
+  {
+    "revision": "ec703aaa4f2a004c160a1e8fb314f14d",
+    "url": "/Cuaderno-de-colecta/static/media/fossils.ec703aaa.png"
+  },
+  {
+    "revision": "299da055bf1d5b28c917f68b51a505db",
+    "url": "/Cuaderno-de-colecta/static/media/insects.299da055.png"
+  },
+  {
+    "revision": "3e82992a347a87dfe9d508853898244e",
+    "url": "/Cuaderno-de-colecta/static/media/plants.3e82992a.png"
+  }
+]);
